efdfe527835d9595e920faa831160ac6d5785678
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-05 15:56:37 +0100

    add empty element pages.

---
+++ src/routes/empty-search/+page.svelte
@@ -0,0 +1,48 @@
+<div class="app-content">
+	<!--====== Section 1 ======-->
+	<div class="u-s-p-y-60">
+		<!--====== Section Content ======-->
+		<div class="section__content">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 u-s-m-b-30">
+						<div class="empty">
+							<div class="empty__wrap">
+								<span class="empty__big-text">SORRY</span>
+
+								<span class="empty__text-1"
+									>Your search, did not match any products. A partial match of your keywords is
+									listed below.</span
+								>
+
+								<span class="empty__text-2"
+									>Related searches:
+
+									<a href="shop-side-version-2.html">men's clothing</a>
+
+									<a href="shop-side-version-2.html">mobiles & tablets</a>
+
+									<a href="shop-side-version-2.html">books & audible</a></span
+								>
+								<form class="empty__search-form">
+									<label for="search-label"></label>
+
+									<input
+										class="input-text input-text--primary-style"
+										type="text"
+										id="search-label"
+										placeholder="Search Keywords"
+									/>
+
+									<button class="btn btn--icon fas fa-search" type="submit"></button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--====== End - Section Content ======-->
+	</div>
+	<!--====== End - Section 1 ======-->
+</div>
